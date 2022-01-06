39a0823dbde3c0df43519deefd8ad09668441878
Stuart Green <github.47a8z@aleeas.com>
2022-01-06 01:29:13 +0000

    removed test dependencies for Ship

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,70 +1,103 @@
 'use strict';
 
 const Ship = require('../src/ship.js');
-const Port = require('../src/port.js');
-const Itinerary = require('../src/itinerary.js');
 
-let itineraryConfig, itinerary, shipConfig, ship;
+let titanicConfig, balticExplorerConfig, titanic, balticExplorer;
 beforeEach(() => {
-    itineraryConfig = {
-        ports: [
-            new Port({ name: 'Southampton'}),
-            new Port({ name: 'Cherbourg' }),
-            new Port({ name: 'Cobh' }),
-            new Port({ name: 'New York City' })
-        ]
+    titanicConfig = {
+        itinerary: {
+            ports: [
+                { name: 'Southampton'},
+                { name: 'Cherbourg'},
+                { name: 'Queenstown'},
+                { name: 'New York City'}
+            ]
+        }
     };
-    itinerary = new Itinerary(itineraryConfig);
-    shipConfig = {
-        itinerary: itinerary
+    balticExplorerConfig = {
+        itinerary: {
+            ports: [
+                { name: 'Southampton'},
+                { name: 'Hamburg'},
+                { name: 'Copenhagen'},
+                { name: 'Visby'},
+                { name: 'Helsinki'},
+                { name: 'St Petersburg'},
+                { name: 'Tallinn'},
+                { name: 'Kiel'},
+                { name: 'Skagen'},
+                { name: 'Southampton'}
+            ]
+        }
     };
-    ship = new Ship(shipConfig);
+    titanic = new Ship(titanicConfig);
+    balticExplorer = new Ship(balticExplorerConfig);
 });
 
-describe('Ship constructor', () => {
-    test('Ship can be instantiated', () => {
-        expect(ship).toBeInstanceOf(Ship);
-    });
-    test('Ship instance has own currentPort property', () => {
-        const { itinerary } = shipConfig;
-        expect(ship.currentPort).toEqual(itinerary.ports[0]);
-    });
-    test('Ship instance has own itinerary property', () => {
-        const { itinerary } = shipConfig;
-        expect(ship.itinerary).toEqual(itinerary);
-    });
-    test('Ship instance has own _alreadyDocked property', () => {
-        expect(ship._alreadyDocked).toEqual([]);
-    });
-});
+describe('Ship', () => {
+    describe('constructor', () => {
+        test('Ship can be instantiated', () => {
+            expect(titanic).toBeInstanceOf(Ship);
+            expect(balticExplorer).toBeInstanceOf(Ship);
+        });
+        test('Ship instances have own "itinerary" property', () => {
+            const { itinerary: titanicItinerary } = titanic;
+            const { itinerary: balticItinerary } = balticExplorer;
 
-describe('setSail method', () => {
-    test('The currentPort property of the Ship instance gets updated', () => {
-        ship.setSail();
-        expect(ship.currentPort).toBe(null);
-    });
-    test('The _alreadyDocked property of the Ship instance gets updated', () => {
-        ship.setSail();
-        expect(ship._alreadyDocked).toEqual([{ name: 'Southampton' }]);
-        ship.setSail();
-        expect(ship._alreadyDocked).toEqual([
-            { name: 'Southampton' },
-            { name: 'Cherbourg' }
-        ]);
-    });
-    test('Return a success message', () => {
-        expect(ship.setSail()).toEqual('The ship has set sail.');
-    });
-});
+            expect(titanic.itinerary).toEqual(titanicItinerary);
+            expect(balticExplorer.itinerary).toEqual(balticItinerary);
+        });
+        test('Ship instances have own "_currentPort" property', () => {
+            const { itinerary: titanicItinerary } = titanic;
+            const { itinerary: balticItinerary } = balticExplorer;
 
-describe('dock method', () => {
-    test('The currentPort property of the Ship instance gets updated', () => {
-        ship.setSail();
-        ship.dock();
-        expect(ship.currentPort).toEqual({ name: 'Cherbourg' });
+            expect(titanic._currentPort).toEqual(titanicItinerary.ports[0]);
+            expect(balticExplorer._currentPort).toEqual(balticItinerary.ports[0]);
+        });
+        test('Ship instances have own "_alreadyDocked" property', () => {
+            expect(titanic._alreadyDocked).toEqual([]);
+            expect(balticExplorer._alreadyDocked).toEqual([]);
+        });
     });
-    test('Return a success message', () => {
-        ship.setSail();
-        expect(ship.dock()).toEqual('The ship has docked at Cherbourg.');
+    describe('setSail', () => {
+        test('State of "_currentPort" should be null', () => {
+            titanic.setSail();
+            expect(titanic._currentPort).toBeNull();
+        });
+        test('State of "itinerary" should change', () => {
+            titanic.setSail();
+            expect(titanic.itinerary).toEqual({
+                ports: [
+                    { name: 'Cherbourg'},
+                    { name: 'Queenstown'},
+                    { name: 'New York City'}
+                ]
+            });
+        });
+        test('State of "_alreadyDocked" should change', () => {
+            expect(titanic._alreadyDocked).toEqual([]);
+            titanic.setSail();
+            expect(titanic._alreadyDocked).toEqual([
+                { name: 'Southampton' }
+            ]);
+            titanic.setSail();
+            expect(titanic._alreadyDocked).toEqual([
+                { name: 'Southampton' },
+                { name: 'Cherbourg' }
+            ]);
+        });
+        test('Returns a success message', () => {
+            expect(titanic.setSail()).toEqual('The ship has set sail.');
+        });
+    });
+    describe('dock', () => {
+        test('State of "_currentPort" should NOT be null', () => {
+            titanic._currentPort = null;
+            titanic.dock();
+            expect(titanic._currentPort).not.toBeNull();
+        });
+        test('Returns a success message', () => {
+            expect(titanic.dock()).toEqual(`The ship has docked at ${titanic.itinerary.ports[0].name}.`);
+        });
     });
 });
--- src/ship.js
+++ src/ship.js
@@ -3,20 +3,20 @@
 class Ship {
     constructor(config) {
         this.itinerary = config.itinerary;
-        this.currentPort = config.itinerary.ports[0];
+        this._currentPort = config.itinerary.ports[0];
         this._alreadyDocked = [];
     }
 
     setSail() {
-        this.currentPort = null;
+        this._currentPort = null;
         const departedPort = this.itinerary.ports.shift();
         this._alreadyDocked.push(departedPort);
         return 'The ship has set sail.';
     }
 
     dock() {
-        this.currentPort = this.itinerary.ports[0];
-        return `The ship has docked at ${this.currentPort.name}.`;
+        this._currentPort = this.itinerary.ports[0];
+        return `The ship has docked at ${this._currentPort.name}.`;
     }
 }
 
