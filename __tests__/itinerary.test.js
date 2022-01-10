00e9de9f5ff0398b1a6c68ac5e446a93a745f720
Stuart Green <github.47a8z@aleeas.com>
2022-01-10 01:11:44 +0000

    test clean up and comment

--- __tests__/itinerary.test.js
+++ __tests__/itinerary.test.js
@@ -3,17 +3,9 @@
 const Itinerary = require('../src/itinerary.js');
 
 describe('Itinerary', () => {
-    let itineraryConfig;
     let itinerary;
     beforeEach(() => {
-        itineraryConfig = {
-            ports: [
-                {
-                    name: jest.fn()
-                }
-            ]
-        };
-        itinerary = new Itinerary(itineraryConfig);
+        itinerary = new Itinerary(jest.fn());
     });
 
     describe('constructor', () => {
--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -3,133 +3,92 @@
 const Ship = require('../src/ship.js');
 
 describe('Ship', () => {
-    let titanicConfig;
-    let balticExplorerConfig;
-    let titanic;
-    let balticExplorer;
-    beforeEach(() => {
-        titanicConfig = {
-            _itinerary: {
-                ports: [
-                    {
-                        name: 'Southampton',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    },
-                    {
-                        name: 'Cherbourg',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    },
-                    {
-                        name: 'Queenstown',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    },
-                    {
-                        name: 'New York City',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    }
-                ]
-            }
-        };
-        balticExplorerConfig = {
-            _itinerary: {
-                ports: [
-                    {
+    describe('constructor', () => {
+        let balticExplorerConfig;
+        let balticExplorer;
+        beforeEach(() => {
+            balticExplorerConfig = {
+                _itinerary: {
+                    ports: [{
                         name: 'Helsinki',
                         _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    },
-                    {
-                        name: 'St Petersburg',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    },
-                    {
-                        name: 'Tallinn',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    },
-                    {
-                        name: 'Kiel',
-                        _ships: [],
-                        addShip: jest.fn(),
-                        removeShip: jest.fn()
-                    }
-                ]
-            }
-        };
-        titanic = new Ship(titanicConfig);
-        balticExplorer = new Ship(balticExplorerConfig);
-    });
+                        addShip: jest.fn()
+                    }]
+                }
+            };
+            balticExplorer = new Ship(balticExplorerConfig);
+        });
 
-    describe('constructor', () => {
         test('Ship can be instantiated', () => {
-            expect(titanic).toBeInstanceOf(Ship);
             expect(balticExplorer).toBeInstanceOf(Ship);
         });
 
         test('Ship instance has own "_isInitialized" property', () => {
-            expect(titanic).toHaveProperty('_isInitialized');
             expect(balticExplorer).toHaveProperty('_isInitialized');
         });
 
         test('Ship instance has own "_itinerary" property', () => {
-            expect(titanic).toHaveProperty('_itinerary');
             expect(balticExplorer).toHaveProperty('_itinerary');
         });
 
         test('Ship instance has own "_currentPort" property', () => {
-            expect(titanic).toHaveProperty('_currentPort');
             expect(balticExplorer).toHaveProperty('_currentPort');
         });
 
         test('Ship instance has own "_previouslyDockedPorts" property', () => {
-            expect(titanic).toHaveProperty('_previouslyDockedPorts');
             expect(balticExplorer).toHaveProperty('_previouslyDockedPorts');
         });
 
-        test('_init is called on Ship instantiation', () => {
-            expect(titanic._isInitialized).toBe(true);
-            expect(balticExplorer._isInitialized).toBe(true);
+        xtest('_init is called on Ship instantiation', () => {
+            Ship.prototype._init = jest.fn();
+            const balticExplorer = new Ship(balticExplorerConfig);
+
+            expect(balticExplorer._init).toHaveBeenCalled();
         });
     });
 
     describe('_init', () => {
+        let balticExplorerConfig;
+        let balticExplorer;
         beforeEach(() => {
-            titanic._isInitialized = false;
-            titanic._currentPort = null;
-            titanic._init();
+            balticExplorerConfig = {
+                _itinerary: {
+                    ports: [{
+                        name: 'Helsinki',
+                        _ships: [],
+                        addShip: jest.fn()
+                    }]
+                }
+            };
+            balticExplorer = new Ship(balticExplorerConfig);
+
+            /*
+                have to reset the values, otherwise the error will always
+                throw because _init is invoked on instantiation.
+            */
+            balticExplorer._isInitialized = false;
+            balticExplorer._currentPort = null;
+            balticExplorer._init();
         });
 
         test('Throws an error if "_isInitialized" is already true', () => {
             const error = 'The ship has already been initialized.';
 
-            titanic._isInitialized = true;
             expect(() => {
-                titanic._init();
+                balticExplorer._init();
             }).toThrowError(error);
         });
 
         test('State of "_currentPort" should NOT be null after _init', () => {
-            expect(titanic._currentPort).not.toBeNull();
+            expect(balticExplorer._currentPort).not.toBeNull();
         });
 
-        test('addShip() should be called when instance is instantiatied', () => {
-            expect(titanic._currentPort.addShip).toBeCalledWith(titanic);
+        test('addShip should be called when instance is instantiatied', () => {
+            expect(balticExplorer._currentPort.addShip).toBeCalledWith(balticExplorer);
         });
 
         test('State of "_isInitialized" should be true after _init', () => {
-            expect(titanic._isInitialized).toBe(true);
+            expect(balticExplorer._isInitialized).toBe(true);
         });
 
         test('State of "_currentPort" should be null if "_itinerary.ports" is empty', () => {
@@ -138,9 +97,9 @@ describe('Ship', () => {
                     ports: []
                 }
             };
-            const titanic = new Ship(emptyConfig);
+            const balticExplorer = new Ship(emptyConfig);
             
-            expect(titanic._currentPort).toBeNull();
+            expect(balticExplorer._currentPort).toBeNull();
         });
 
         test('State of "_isInitialized" should be false if "_itinerary.ports" is empty', () => {
@@ -149,110 +108,154 @@ describe('Ship', () => {
                     ports: []
                 }
             };
-            const titanic = new Ship(emptyConfig);
+            const balticExplorer = new Ship(emptyConfig);
 
-            expect(titanic._isInitialized).toBe(false);
+            expect(balticExplorer._isInitialized).toBe(false);
         });
     });
 
     describe('_setSail', () => {
+        let balticExplorerConfig;
+        let balticExplorer;
+        beforeEach(() => {
+            balticExplorerConfig = {
+                _itinerary: {
+                    ports: [
+                        {
+                            name: 'Helsinki',
+                            _ships: [],
+                            addShip: jest.fn(),
+                            removeShip: jest.fn()
+                        },
+                        {
+                            name: 'St Petersburg',
+                            _ships: [],
+                            addShip: jest.fn(),
+                            removeShip: jest.fn()
+                        }
+                    ]
+                }
+            };
+            balticExplorer = new Ship(balticExplorerConfig);
+        });
+
         test('Throws an error if "_itinerary.ports" is empty', () => {
             const error = 'The ship has no itinerary.';
 
-            titanic._itinerary.ports = [];
+            balticExplorer._itinerary.ports = [];
             expect(() => {
-                titanic._setSail();
+                balticExplorer._setSail();
             }).toThrowError(error);
         });
 
         test('Throws an error if "_currentPort" is null', () => {
             const error = 'The ship has already set sail.';
 
-            titanic._currentPort = null;
+            balticExplorer._currentPort = null;
             expect(() => {
-                titanic._setSail();
+                balticExplorer._setSail();
             }).toThrowError(error);
         });
 
-        test('Throws an error if "_itinerary.ports" has one item, and "_previouslyDockedPorts" is not empty', () => {
+        test('Throws an error if "_itinerary.ports" has one port AND "_previouslyDockedPorts" is not empty', () => {
             const error = 'The ship has already set sail.';
 
-            titanic._itinerary.ports = [];
-            titanic._itinerary.ports.push(jest.fn());
-            titanic._previouslyDockedPorts.push(jest.fn());
+            balticExplorer._itinerary.ports = [jest.fn()];
+            balticExplorer._previouslyDockedPorts.push(jest.fn());
             expect(() => {
-                titanic._setSail();
+                balticExplorer._setSail();
             }).toThrowError(error);
         });
 
         test('State of "_currentPort" should be set to null', () => {
-            expect(titanic._currentPort).not.toBeNull();
-            titanic._setSail();
-            expect(titanic._currentPort).toBeNull();
-        });
-
-        // look at this again... serialization error when testing array value with toEqual (caused by jest.fn)
-        test('State of "_itinerary" should change, the array removes one element each time', () => {
-            expect(titanic._itinerary.ports.length).toBe(4);
-            titanic._setSail();
-            expect(titanic._itinerary.ports.length).toBe(3);
-        });
-
-        test('removeShip() should be invoked from within _setSail()', () => {
-            titanic._setSail();
-            const titanicDepartedPort = titanic._previouslyDockedPorts[0];
-            expect(titanicDepartedPort.removeShip).toHaveBeenCalledWith(titanic);
-
+            expect(balticExplorer._currentPort).not.toBeNull();
             balticExplorer._setSail();
-            const balticDepartedPort = balticExplorer._previouslyDockedPorts[0];
-            expect(balticDepartedPort.removeShip).toHaveBeenCalledWith(balticExplorer);
+            expect(balticExplorer._currentPort).toBeNull();
         });
 
-        // look at this again... serialization error when testing array value with toEqual (caused by jest.fn)
-        test('State of "_previouslyDockedPorts" should change, the array adds one element each time', () => {
-            expect(titanic._previouslyDockedPorts.length).toBe(0);
-            titanic._setSail();
-            expect(titanic._previouslyDockedPorts.length).toBe(1);
+        test('Remove one port from "_itinerary.ports"', () => {
+            expect(balticExplorer._itinerary.ports.length).toBe(2);
+            expect(balticExplorer._itinerary.ports[0].name).toBe('Helsinki');
+            expect(balticExplorer._itinerary.ports[1].name).toBe('St Petersburg');
+            balticExplorer._setSail();
+            expect(balticExplorer._itinerary.ports.length).toBe(1);
+            expect(balticExplorer._itinerary.ports[0].name).toBe('St Petersburg');
+        });
+
+        test('removeShip should be called when _setSail is invoked', () => {
+            balticExplorer._setSail();
+            const [departedPort] = balticExplorer._previouslyDockedPorts;
+            expect(departedPort.removeShip).toHaveBeenCalledWith(balticExplorer);
+        });
+
+        test('Add the removed port from "_itinerary.ports" to "_previouslyDockedPorts"', () => {
+            expect(balticExplorer._previouslyDockedPorts.length).toBe(0);
+            expect(balticExplorer._previouslyDockedPorts).toEqual([]);
+            balticExplorer._setSail();
+            expect(balticExplorer._previouslyDockedPorts.length).toBe(1);
+            expect(balticExplorer._previouslyDockedPorts[0].name).toEqual('Helsinki');
         });
 
         test('Returns a success message', () => {
-            expect(titanic._setSail()).toEqual(
-                `The ship has set sail! Next stop: ${titanic._itinerary.ports[0].name}.`
+            expect(balticExplorer._setSail()).toEqual(
+                `The ship has set sail! Next stop: ${balticExplorer._itinerary.ports[0].name}.`
             );
         });
     });
 
     describe('_dock', () => {
+        let balticExplorerConfig;
+        let balticExplorer;
         beforeEach(() => {
-            titanic._previouslyDockedPorts = [jest.fn()];
+            balticExplorerConfig = {
+                _itinerary: {
+                    ports: [
+                        {
+                            name: 'Helsinki',
+                            _ships: [],
+                            addShip: jest.fn(),
+                            removeShip: jest.fn()
+                        },
+                        {
+                            name: 'St Petersburg',
+                            _ships: [],
+                            addShip: jest.fn(),
+                            removeShip: jest.fn()
+                        }
+                    ]
+                }
+            };
+            balticExplorer = new Ship(balticExplorerConfig);
+
+            /*
+                ensure _previouslyDockedPorts is not empty otherwise
+                the error will always throw.
+            */
             balticExplorer._previouslyDockedPorts = [jest.fn()];
         });
 
         test('Throws an error if "_previouslyDockedPorts" is empty', () => {
             const error = 'You must set sail before you can dock.';
 
-            titanic._previouslyDockedPorts = [];
+            balticExplorer._previouslyDockedPorts = [];
             expect(() => {
-                titanic._dock();
+                balticExplorer._dock();
             }).toThrowError(error);
         });
 
         test('State of "_currentPort" should NOT be null once docked', () => {
-            titanic._dock();
-            expect(titanic._currentPort).not.toBeNull();
+            balticExplorer._dock();
+            expect(balticExplorer._currentPort).not.toBeNull();
         });
 
-        test('addShip() should be invoked from within _dock()', () => {
-            titanic._dock();
-            expect(titanic._currentPort.addShip).toHaveBeenCalledWith(titanic);
-
+        test('addShip should be called when _dock is invoked', () => {
             balticExplorer._dock();
             expect(balticExplorer._currentPort.addShip).toHaveBeenCalledWith(balticExplorer);
         });
 
         test('Returns a success message', () => {
-            expect(titanic._dock()).toEqual(
-                `The ship has docked at ${titanic._itinerary.ports[0].name}.`
+            expect(balticExplorer._dock()).toEqual(
+                `The ship has docked at ${balticExplorer._itinerary.ports[0].name}.`
             );
         });
     });
